c10916d356c7ff63f2d2cafebeb81fa5f30fbfb2
itj3 <itj3@nau.edu>
2015-04-23 14:27:51 -0700

    second function created
    takes an accusation ID and returns the dean associated with its course
    section.

--- functions.sql
+++ functions.sql
@@ -12,4 +12,29 @@ BEGIN
 	RETURN has_cheated;
 END check_student;
 /
---this works but I have no idea how to call it...
+
+--takes an accusation id and returns the dean associated with the course section
+--the student(s) were accused cheating in.
+CREATE OR REPLACE FUNCTION get_dean(accu_id Accusation.acc_id%type)
+RETURN varchar2
+IS
+result varchar2(21);
+BEGIN
+	SELECT dean INTO result 
+	FROM ((((College INNER JOIN Department USING (col_id))
+	             	 INNER JOIN Course USING (dept_id)) 
+	             	 INNER JOIN CourseSection USING (cou_id)) 
+	             	 INNER JOIN Accusation USING (cs_id)) 
+	WHERE acc_id = accu_id;
+END get_dean;
+/
+
+--if the inner joins don't make sense it might be easier to represent in a nested select
+/*
+SELECT dean INTO result FROM College WHERE col_id IN
+		(SELECT col_id FROM Department WHERE dept_id IN (
+			SELECT dept_id FROM Course WHERE cou_id IN (
+				SELECT cou_id FROM CourseSection WHERE cs_id IN (
+					SELECT cs_id FROM Accusation WHERE acc_id = accud_id))));
+*/
+
