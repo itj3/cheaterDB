7ef50085db6e509e908f4d2e83da737f0c65bef7
Riley Shelton <rs854@nau.edu>
2015-04-22 23:18:05 -0700

    Grants Permission
    I just need the names of the views, functions, and procedures before
    this is complete.

---
+++ grant.sql
@@ -0,0 +1,19 @@
+--Gives complete privilege on all tables/views
+--Tables
+Grant ALL ON Student to mlh395;
+Grant ALL ON Instructor to mlh395;
+Grant ALL ON College to mlh395;
+Grant ALL ON Department to mlh395;
+Grant ALL ON CourseSection to mlh395;
+Grant ALL ON ClassSchedule to mlh395;
+Grant ALL ON Course to mlh395;
+Grant ALL ON Accusation to mlh395;
+--Views
+Grant ALL ON /*name*/ to mlh395;
+Grant ALL ON /*name*/ to mlh395;
+
+--Gives execution privileges on all functions and procedures.
+--needs the names functions and procedures
+Grant Execute On /*name*/ to mlh395;
+Grant Execute On /*name*/ to mlh395;
+Grant Execute On /*name*/ to mlh395;
