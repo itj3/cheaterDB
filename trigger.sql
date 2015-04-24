c8962fff305bb96aa8282ab87160911ada72fcfc
Grant570 <gs339@nau.edu>
2015-04-24 16:16:22 -0700

    fixed trigger issues kinda
    changed to for each row and updated date

--- trigger.sql
+++ trigger.sql
@@ -1,8 +1,11 @@
+Drop Trigger r_acc_before_delete_trig;
+
 Create Trigger r_acc_before_delete_trig
 Before Delete on Accusation
+For Each Row 
 Begin
-	if to_number(to_char(sysdate, 'YYYY')+7) not in acc_date
+	if to_date(add_months(:old.acc_date,84)) != to_date(sysdate, 'DD-MON-YYYY') then
 		raise_application_error(-20500, 'This record can not be deleted, it is still within the retention time period');
 	end if;
-END; r_accusation_before_delete_trigger
+END;
 /
