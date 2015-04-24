a1226e0769dd09e32352f0c1e7fae20cb70f8689
Riley Shelton <rs854@nau.edu>
2015-04-24 14:28:46 -0700

    Drop
    Simple way to drop the tables

---
+++ drop.sql
@@ -0,0 +1,13 @@
+Drop table Accusation;
+Drop table Course;
+Drop table ClassSchedule;
+Drop table CourseSection;
+Drop table Department;
+Drop table College;
+Drop table Instructor;
+Drop table Student;
+
+
+
+
+
