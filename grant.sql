72866d50c09d64db9eb9a89b2c08c15569564931
Riley Shelton <rs854@nau.edu>
2015-04-24 14:33:46 -0700

    Updated the Grants

--- grant.sql
+++ grant.sql
@@ -8,12 +8,13 @@ Grant ALL ON CourseSection to mlh395;
 Grant ALL ON ClassSchedule to mlh395;
 Grant ALL ON Course to mlh395;
 Grant ALL ON Accusation to mlh395;
+Grant All On r_acc_before_delete_trig to mlh395;
 --Views
 Grant ALL ON allaccu to mlh395;
 Grant ALL ON pending to mlh395;
 
 --Gives execution privileges on all functions and procedures.
 --needs the names functions and procedures
-Grant Execute On /*name*/ to mlh395;
-Grant Execute On /*name*/ to mlh395;
-Grant Execute On /*name*/ to mlh395;
+Grant Execute On add_accusation to mlh395;
+Grant Execute On check_student to mlh395;
+Grant Execute On get_dean to mlh395;
